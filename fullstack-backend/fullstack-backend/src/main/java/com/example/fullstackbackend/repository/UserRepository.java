0f27d24fe9a0e11e1e291f4217dd4ad7b7649175
MenukaRanasinghe <88645663+MenukaRanasinghe@users.noreply.github.com>
2023-04-03 15:32:55 +0530

    Delete fullstack-backend/fullstack-backend directory

--- fullstack-backend/fullstack-backend/src/main/java/com/example/fullstackbackend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fullstackbackend.repository;
-
-import com.example.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
